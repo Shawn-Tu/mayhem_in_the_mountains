40e47b4c8f324d818ac946cc6385447f129b9431
Shawn-Tu <100446734+Shawn-Tu@users.noreply.github.com>
2023-01-18 11:34:04 -0800

    basic AI
    I have added a basic AI that follows the player.

---
+++ mayheminmountains/Assets/Scripts/LineOfSight.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LineOfSight : MonoBehaviour
+{
+    public float distance;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit2D hitInfo = Physics2D.Raycast(transform.position, transform.right, distance);
+        if(hitInfo.collider != null)
+        {
+            Debug.DrawLine(transform.position, hitInfo.point, Color.red);
+        }
+        else
+        {
+            Debug.DrawLine(transform.position, transform.position + transform.right * distance, Color.green);
+        }
+
+    }
+}
