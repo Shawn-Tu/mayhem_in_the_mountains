7c250dcf1d452b8545f753b5c8b802efe6caa155
Shawn-Tu <100446734+Shawn-Tu@users.noreply.github.com>
2023-01-26 20:29:20 -0800

    Knockback change
    Knockback change. Yep it works now

--- mayheminmountains/Assets/Scripts/Knockback.cs
+++ mayheminmountains/Assets/Scripts/Knockback.cs
@@ -6,32 +6,26 @@ using UnityEngine.Events;
 
 public class Knockback : MonoBehaviour
 {
-    public Rigidbody2D rb2d;
-    public float thrust;
     public bool Contact = false;
     public Vector2 difference = new Vector2(10f, 10);
-    public int divide = 50;
-    public int count = 50;
+    public int divide;
+    public int count = 10;
     public float differenceX;
     public float differenceY;
-
-    //Vector2 small = new Vector2(0.01f, 0.01f);
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.tag == "Damage")
         {
             Contact = true;
-            //difference = (transform.position - other.transform.position);
+            difference = (transform.position - other.transform.position);
             differenceX = difference.x / divide;
             differenceY = difference.y / divide;
-            //Debug.Log(difference.x + " " + difference.y);
         }
     }
 
     private void Update()
     {
-        Debug.Log(difference.y);
         if (Contact && count > 0)
         {
             count--;
@@ -41,7 +35,6 @@ public class Knockback : MonoBehaviour
         {
             Contact=false;
             count = 10;
-            difference = new Vector2(10f, 10f);
         }
     }
 }
