43e5a2ad7a9de6e8373bb998060b31e31901eb9f
Shawn-Tu <100446734+Shawn-Tu@users.noreply.github.com>
2023-04-26 11:30:39 -0700

    Added transitions between levels, and bosses.

--- mayheminmountains/Assets/Scripts/SceneChange.cs
+++ mayheminmountains/Assets/Scripts/SceneChange.cs
@@ -6,20 +6,13 @@ using UnityEngine.SceneManagement;
 public class SceneChange : MonoBehaviour
 {
     public int sceneBuildIndex;
-    public bool isNextScene = true;
-
-    [SerializeField]
-    public SceneInfo sceneInfo;
 
     private void OnTriggerEnter2D(Collider2D col)
     {
-        sceneInfo.isNextScene = isNextScene;
-        print("Trigger Entered");
-
         if(col.tag == "Player")
         {
             print("Switching Scene to " + sceneBuildIndex);
-            SceneManager.LoadScene(sceneBuildIndex, LoadSceneMode.Single);
+            SceneManager.LoadScene(sceneBuildIndex + 1, LoadSceneMode.Single);
         }
     }
 }
